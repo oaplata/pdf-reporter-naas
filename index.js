8c078e1f5b5f295d55d85e7c307e16503fca09c1
Omar Plata <omar970329@gmail.com>
2023-12-20 10:54:58 -0500

    no sandbox

--- index.js
+++ index.js
@@ -11,7 +11,10 @@ const htmlSettlementLetter = require('./templates/settlement/settlementLetter');
 
 
 async function htmlToPDFBuffer(html, landscape = false) {
-    const browser = await puppeteer.launch({ headless: 'new' });
+    const browser = await puppeteer.launch({
+      headless: 'new',
+      args: ['--no-sandbox', '--disable-setuid-sandbox']
+    });
     const page = await browser.newPage();
     await page.setContent(html);
 
